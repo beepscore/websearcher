3910199b2a670def5d604e89ab2450ec4a9b68f7
beepscore <support@beepscore.com>
2014-09-06 20:57:12 -0700

    In test_arg_reader.py add test_args_from_argument.

--- test_arg_reader.py
+++ test_arg_reader.py
@@ -24,6 +24,31 @@ class TestArgReader(unittest.TestCase):
         self.assertEqual(2, args.item_end, '')
         self.assertEqual("./search_results.txt", args.outfile, '')
 
+    def test_args_from_argument(self):
+        reader = arg_reader.ArgReader()
+
+        test_expression = "foo"
+        test_uri_start = "bar"
+        test_uri_end = "baz"
+        test_item_start = "3"
+        test_item_end = "7"
+        test_outfile = "pigs"
+
+        test_commandline = ["-expression", test_expression,
+                            "-uri_start", test_uri_start,
+                            "-uri_end", test_uri_end,
+                            "-item_start", test_item_start,
+                            "-item_end", test_item_end,
+                            "-outfile", test_outfile
+                            ]
+        args = reader.args(test_commandline)
+
+        self.assertEqual(test_expression, args.expression, '')
+        self.assertEqual(test_uri_start, args.uri_start, '')
+        self.assertEqual(test_uri_end, args.uri_end, '')
+        self.assertEqual(test_item_start, args.item_start, '')
+        self.assertEqual(test_item_end, args.item_end, '')
+        self.assertEqual(test_outfile, args.outfile, '')
 
 if __name__ == "__main__":
     unittest.main()
