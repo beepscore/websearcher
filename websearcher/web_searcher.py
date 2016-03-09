2121beb5e38f86b8e005a0078391af96869aeeb7
beepscore <support@beepscore.com>
2016-03-08 23:05:12 -0800

    Add docstrings.

--- websearcher/web_searcher.py
+++ websearcher/web_searcher.py
@@ -54,18 +54,29 @@ class WebSearcher():
         :param argfile: file with arguments. Don't version control argfile. Put it outside project directory.
         :return: None
         """
-
         self.arg_reader = arg_reader.ArgReader()
         self.args = self.arg_reader.args([argfile])
         self.page_reader = page_reader.PageReader()
 
     def request_url_write_to_out_file(self, url, out_file):
+        """
+        Request url and write response.
+
+        :param url: url to request
+        :param out_file: filename to write response to
+        :return: None
+        """
         response = self.page_reader.response(url)
 
         writer = file_writer.FileWriter(self.args.out_directory, out_file, response.text)
         writer.create_file(writer.dirname, writer.filename, writer.content)
 
     def request_url_write_to_file(self, url):
+        """
+        Request url, write response to file name based on filename_from_url
+        :param url: url to request
+        :return: None
+        """
         out_file = file_writer.FileWriter.filename_from_url(url)
         self.request_url_write_to_out_file(url, out_file)
 
