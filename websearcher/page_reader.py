9850f778c2328312f811d0de11cddbae813ad7ca
beepscore <support@beepscore.com>
2016-05-31 00:45:53 -0700

    In page_reader.py use webdriver to select element and beautiful soup to parse it

--- websearcher/page_reader.py
+++ websearcher/page_reader.py
@@ -54,19 +54,44 @@ class PageReader:
         """
         Request web page at url, return links whose class matches "spell"
         """
-        #response = self.response(url)
-        # html_doc = response.text
-        # return self.class_name_from_html(html_doc, "spell")
-
         browser = webdriver.Firefox()
         browser.get(url)
 
-            # http://stackoverflow.com/questions/31064528/webdriver-how-to-find-elements-when-class-name-contains-space
+        # http://stackoverflow.com/questions/37422832/waiting-for-a-page-to-load-in-selenium-firefox-w-python?lq=1
+        # http://stackoverflow.com/questions/5868439/wait-for-page-load-in-selenium
+        WebDriverWait(browser, 10).until(lambda d: d.find_element_by_class_name("spell").is_displayed())
+
+        # example google search html
+        # <p class="sp_cnt card-section">
+        # <span class="spell">Showing results for</span>
+        # <a class="spell" href="/search?/search?biw=1280&bih=423&q=tuberculosis&spell=1&sa=X&ved=0ahUKEwjMyeG30oPNAhVMz2MKHRw5D10QvwUIGSgA">
+        # <b>
+        # <i>tuberculosis</i>
+        # </b>
+        # </a>
+
+        # use find_element_by_css_selector to match compound class (2 classes)
+        # http://stackoverflow.com/questions/17808521/how-to-avoid-compound-class-name-error-in-page-object
+        sp_cnt_card_section = browser.find_element_by_css_selector(".sp_cnt.card-section")
+
+        spell_elems = sp_cnt_card_section.find_elements_by_class_name("spell")
+        spell_elem = None
+        for elem in spell_elems:
+            if elem.tag_name == "a":
+                spell_elem = elem
+
+        # e.g. <b><i>asthma</i></b>
+        spell_link_text = spell_elem.get_attribute('innerHTML')
+        browser.quit()
+
+        soup = BeautifulSoup(spell_link_text, 'html.parser')
+        return soup.i.contents[0]
+
+"""
         try:
-            taw_elem = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, "taw")))
-            # print('taw_elem ' % (taw_elem))
-            spell_elems = browser.find_elements_by_class_name("spell")
-            print("len(spell_elems) == {0}".format(len(spell_elems)))
+            # taw_elem = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, "taw")))
+            # spell_elems = browser.find_elements_by_class_name("spell")
+            # print("len(spell_elems) == {0}".format(len(spell_elems)))
 
             try:
                 #spell_elem = browser.find_element_by_class_name("spell")
@@ -80,4 +105,5 @@ class PageReader:
             browser.quit()
             spell_elem = spell_elems[1]
             return spell_elem
+"""
 
