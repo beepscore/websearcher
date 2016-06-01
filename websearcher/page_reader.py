f2abc8681fa76b4d9421197b730013d371c84a23
beepscore <support@beepscore.com>
2016-05-31 17:50:53 -0700

    Delete unneeded import

--- websearcher/page_reader.py
+++ websearcher/page_reader.py
@@ -2,7 +2,6 @@
 
 import requests
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
