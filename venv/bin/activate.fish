05dda67214a3ce5020992266cd48b3a999723989
Katherine Little <klittle@edaya.com>
2016-06-03 22:31:36 -0700

    After clonining, deleted ./venv and re-ran pyvenv venv. Now source ./venv/bin/activate works again

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelavent variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/stevebaker/Documents/projects/pythonProjects/websearcher/venv"
+set -gx VIRTUAL_ENV "/Users/klittle/Documents/pythonProjects/websearcher/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
