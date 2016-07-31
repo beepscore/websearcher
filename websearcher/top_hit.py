c3edc06606bbc754fb2bad3e61582516e32f3d78
beepscore <support@beepscore.com>
2016-07-31 11:16:36 -0700

    Revise print statements

--- websearcher/top_hit.py
+++ websearcher/top_hit.py
@@ -71,16 +71,18 @@ class TopHit:
         with open(in_file_full_path, 'r') as input_file, open(out_file_full_path, 'w') as output_file:
             line_number = 1
             for line in input_file.readlines():
-                print('input line ' + str(line_number) + ' ' + line)
+                print('input line number: ' + str(line_number) + ' line :' + line)
                 search_string = line.split(",")[0]
 
                 count = ""
                 if line is not None and len(line.split(",")) > 1:
                     count = line.split(",")[1]
 
-                print("searching " + search_string)
+                print("searching...")
                 search_result = self.top_hit(search_string)
                 search_result_line = search_string + "," + count + "," + search_result
-                print("output line " + search_result_line)
+                print("output: ")
+                print(search_result_line)
+                print()
                 output_file.write(search_result_line + '\n')
                 line_number += 1
