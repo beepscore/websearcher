993c5cc7e3cf82930436f1c66fe1d77b64f46864
beepscore <support@beepscore.com>
2016-08-02 18:54:55 -0700

    Rename spelling_suggester.py to spelling_suggestions.py

--- get_suggested_spellings.py
+++ get_suggested_spellings.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from websearcher import spelling_suggester
+from websearcher import spelling_suggestions
 
 """
 Search words in input file. Use command line arguments.
 """
 
-suggester = spelling_suggester.SpellingSuggester("@./websearcher_data/inputs/spelling_suggester_args.txt")
+suggester = spelling_suggestions.SpellingSuggester("@./websearcher_data/inputs/spelling_suggester_args.txt")
 actual = suggester.suggested_spellings_from_file()
 print("results")
 print('\n'.join(actual))
