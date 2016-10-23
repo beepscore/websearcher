b43dbf1e1c5cdafeb02ea18a0dcb2b034b56dc56
beepscore <support@beepscore.com>
2016-10-23 10:04:53 -0700

    Don't print results from suggested_spellings_from_file(), it doesn't return anything, just writes to a file

--- get_suggested_spellings.py
+++ get_suggested_spellings.py
@@ -7,6 +7,4 @@ Search words in input file. Use command line arguments.
 """
 
 suggester = spelling_suggester.SpellingSuggester("@./websearcher_data/inputs/spelling_suggester_args.txt")
-actual = suggester.suggested_spellings_from_file()
-print("results")
-print('\n'.join(actual))
+suggester.suggested_spellings_from_file()
