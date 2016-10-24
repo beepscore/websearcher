20966d8be6ba16d584011586aed90ef985d87a0c
beepscore <support@beepscore.com>
2016-10-23 19:56:39 -0700

    Change another pyethon to pythan

--- tests/test_spelling_suggester.py
+++ tests/test_spelling_suggester.py
@@ -14,7 +14,7 @@ class TestSpellingSuggester(unittest.TestCase):
 
     def test_suggested_spellings(self):
         suggester = spelling_suggester.SpellingSuggester("@./websearcher_data/inputs/spelling_suggester_args.txt")
-        actual = suggester.suggested_spellings(["pyethon", "python", "javascwipt", "swoft"])
+        actual = suggester.suggested_spellings(["pythan", "python", "javascwipt", "swoft"])
         expected = ["python", "", "javascript", "swift"]
         self.assertEqual(expected, actual, '')
 
