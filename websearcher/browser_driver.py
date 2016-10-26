bed1206df84be13978df5975ad343aa098759bab
beepscore <support@beepscore.com>
2016-10-25 20:38:55 -0700

    Move st_html from top_hit.py to browser_driver.py

--- websearcher/browser_driver.py
+++ websearcher/browser_driver.py
@@ -44,3 +44,38 @@ def taw_html(search_string):
 
     finally:
         browser.quit()
+
+
+def st_html(search_string):
+    """
+    Use browser to search for a term
+    wait for javascript to run and return html for class "st"
+    return empty string if browser doesn't suggest a spelling
+    """
+    browser = webdriver.Firefox()
+
+    # google
+    base_url = "https://www.google.com"
+    query_prefix = "/#q="
+
+    url = base_url + query_prefix + search_string
+    browser.get(url)
+
+    try:
+        # http://stackoverflow.com/questions/37422832/waiting-for-a-page-to-load-in-selenium-firefox-w-python?lq=1
+        # http://stackoverflow.com/questions/5868439/wait-for-page-load-in-selenium
+        WebDriverWait(browser, 6).until(lambda d: d.find_element_by_class_name("st").is_displayed())
+        st = browser.find_element_by_class_name("st")
+        inner_html = st.get_attribute('innerHTML')
+        return inner_html
+
+    except selenium.common.exceptions.TimeoutException:
+        print("TimeoutException, returning empty string")
+        return ""
+
+    except AttributeError:
+        print("AttributeError, returning empty string")
+        return ""
+
+    finally:
+        browser.quit()
--- websearcher/top_hit.py
+++ websearcher/top_hit.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
-
 from bs4 import BeautifulSoup
+from websearcher import browser_driver
 
 
 def top_hit(search_string):
@@ -12,45 +9,10 @@ def top_hit(search_string):
     Use browser to search for a term and return top hit
     return empty string if browser doesn't suggest a spelling
     """
-    html = st_html(search_string)
+    html = browser_driver.st_html(search_string)
 
     parsed = parsed_text(html)
     return parsed
-
-
-def st_html(search_string):
-    """
-    Use browser to search for a term
-    wait for javascript to run and return html for class "st"
-    return empty string if browser doesn't suggest a spelling
-    """
-    browser = webdriver.Firefox()
-
-    # google
-    base_url = "https://www.google.com"
-    query_prefix = "/#q="
-
-    url = base_url + query_prefix + search_string
-    browser.get(url)
-
-    try:
-        # http://stackoverflow.com/questions/37422832/waiting-for-a-page-to-load-in-selenium-firefox-w-python?lq=1
-        # http://stackoverflow.com/questions/5868439/wait-for-page-load-in-selenium
-        WebDriverWait(browser, 6).until(lambda d: d.find_element_by_class_name("st").is_displayed())
-        st = browser.find_element_by_class_name("st")
-        inner_html = st.get_attribute('innerHTML')
-        return inner_html
-
-    except selenium.common.exceptions.TimeoutException:
-        print("Didn't find element")
-        return ""
-
-    except AttributeError:
-        print("Didn't find element")
-        return ""
-
-    finally:
-        browser.quit()
 
 
 def parsed_text(html):
